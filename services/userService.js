fedab494b2bf35a25b21dc527d156e8ca3d8afab
SaraRRodrigues <sarafof@hotmail.com>
2018-06-17 01:07:55 +0100

    update#4 on userService.js

--- services/userService.js
+++ services/userService.js
@@ -7,7 +7,7 @@ var connectDB = "postgres://BiAMa:1234@localhost/BiAMaDB";
 const { Client } = require('pg');
 
 const client = new Client({
-	connectionString: process.env.DATABASE_URI_HEROKU,
+	connectionString: process.env.DATABASE_URL,
 	ssl: true,
 });
   
