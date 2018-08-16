3a312c8423958f82c19e2cef76a4e3980841ea88
SaraRRodrigues <sarafof@hotmail.com>
2018-08-16 14:13:37 +0100

    favorites user nav details

--- public/controllers/favorites-controller.js
+++ public/controllers/favorites-controller.js
@@ -1,4 +1,4 @@
-app.controller('FavoritesController',['$scope', "$http", "FavoritesService", "LibraryMaterialInfoService","QuestionFavoriteService","$route", "$sce", function($scope, $http, FavoritesService, LibraryMaterialInfoService,QuestionFavoriteService,$route, $sce) {
+app.controller('FavoritesController',['$scope', "$http", "FavoritesService", "LibraryMaterialInfoService","QuestionFavoriteService","UserFavoriteService","$route", "$sce", function($scope, $http, FavoritesService, LibraryMaterialInfoService,QuestionFavoriteService,UserFavoriteService,$route, $sce) {
     
     /* hide footer of index page because of click in buttons footer reload page */
 	jQuery("#footerMain").hide();
@@ -94,7 +94,23 @@ app.controller('FavoritesController',['$scope', "$http", "FavoritesService", "Li
             $scope.showMaterials = true;
         }
     }*/
+    $scope.clickUserDetails = function() {
+		if($scope.userDetails){
+			$scope.userDetails = false;
+		}else {
+			$scope.userDetails = true;
+			$scope.showSearch = false;
+		}
+    }
 
+    $scope.showInitSessionDiv = function () {
+		if($scope.showInitSession){
+			$scope.showInitSession = false;
+		}else {
+			$scope.showInitSession = true;
+		}
+    }
+    
     $scope.clickTopSearch = function() {
 		if($scope.showSearch){
 			$scope.showSearch = false;
@@ -103,6 +119,80 @@ app.controller('FavoritesController',['$scope', "$http", "FavoritesService", "Li
 		}
 	}
 
+    $scope.confirmSessionAction = function (username, password) {
+
+		$scope.users = 'loadUser';
+		var getAllUsers = UserFavoriteService.getUsers(function(users){});
+		
+		getAllUsers.then(function(usersDB) {
+			$scope.users = usersDB.data.users;
+			for(var index=0; index<$scope.users.length; ++index){
+				$scope.userName = $scope.users[index].username;
+				$scope.userPassword = $scope.users[index].password;
+				$scope.userImage = $scope.users[index].image;
+				$scope.userEmail = $scope.users[index].email;
+				$scope.nameUser=$scope.users[index].name;
+				$scope.userBirthdate = $scope.users[index].birthdate;
+
+				var splitDateBirth = $scope.userBirthdate.split('/');
+				$scope.dayBirth = splitDateBirth[0];
+				$scope.monthBirth = splitDateBirth[1];
+				$scope.yearBirth = splitDateBirth[2];
+
+				if($scope.userName !== null && $scope.userName === username){
+					if($scope.userPassword !== null && $scope.userPassword === password){
+						$scope.userLoggedIn=$scope.users[index].username;
+						$scope.idUserLoggerIn=$scope.users[index].id;
+						$scope.confirmSession = true;
+						break;
+					}
+				}
+			}
+		});
+    }
+
+    $scope.disableSearch = function(buttonClick) {
+
+		if($scope.isMobileView) {
+			if(buttonClick === 'favorites') {
+				location.href = 'http://localhost:8080/BiAMa/favoritesMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'questions') {
+				location.href = 'http://localhost:8080/BiAMa/myQuestionsMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'world_share') {
+				location.href = 'http://localhost:8080/BiAMa/worldShareMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'notification') {
+				location.href = 'http://localhost:8080/BiAMa/notificationsMobile?userName=' + $scope.idUserLoggerIn;
+	
+			}
+	
+			if(buttonClick == 'perfil') {
+				location.href = 'http://localhost:8080/BiAMa/perfilPageMobile?userId=' + $scope.idUserLoggerIn + '&userName=' 
+				+ $scope.userName + '&userPassword=' + $scope.userPassword + '&userImage=' + $scope.userImage + '&userBirthdate=' + $scope.dayBirth + '-' + $scope.monthBirth + '-' + $scope.yearBirth 
+				+ '&nameUser=' + $scope.nameUser + '&userEmail=' + $scope.userEmail;
+			}
+	
+			if(buttonClick == 'compare') {
+				location.href = 'http://localhost:8080/BiAMa/compareMobile?userName=' + $scope.idUserLoggerIn;
+	
+			}
+		}
+		
+		if(buttonClick === 'notification') {
+			$scope.userDetails = true;
+			$scope.notificationNumber=true;
+		} else {
+			$scope.userDetails = false;
+			$scope.notificationNumber = false;
+		}
+		$scope.search = false;
+	}
+    
     $scope.reload = function() {
         $route.reload();
     }
@@ -401,6 +491,96 @@ app.factory("LibraryMaterialInfoService", function($q, $http, $timeout){
         getSchoolOfMaterial: getSchoolOfMaterial,
         getMaterial: getMaterial
     };
+});
+
+app.factory("UserFavoriteService", function($q, $http, $timeout){
+    
+	var getUsers = function() {
+		var deferred = $q.defer();
+	
+ 		$timeout(function() {
+		  deferred.resolve($http.get('/users',  {cache:true}));
+		}, 2000); 
+	
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			deferred.resolve(resp);
+		}
+
+		xhr.open('GET','/users', true);
+		xhr.send();*/
+
+		return deferred.promise;
+	};
+
+	var getMyQuestionsLogged = function() {
+		var deferred = $q.defer();
+
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			deferred.resolve(resp);
+		}
+
+		xhr.open('GET','/myQuest', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.get('/myQuest'));
+		}, 2000);
+
+		return deferred.promise;
+	};
+
+	var insertLibraryUserDetails = function() {
+		var deferred = $q.defer();
+
+		
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			var response = resp.response;
+			deferred.resolve(response);
+		}
+
+		xhr.open('GET','/insertLibraryUser', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.post('/insertLibraryUser'));
+		}, 2000);
+
+		return deferred.promise;
+	}
+
+	var getLibraryUserDetails = function() {
+		
+		var deferred = $q.defer();
+
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			var response = resp.response;
+			deferred.resolve(response);
+		}
+
+		xhr.open('GET','/getLibraryUser', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.get('/getLibraryUser'));
+		}, 2000);
+
+		return deferred.promise;
+	}
+
+	return {
+		getUsers: getUsers,
+		getMyQuestionsLogged: getMyQuestionsLogged,
+		insertLibraryUserDetails: insertLibraryUserDetails,
+		getLibraryUserDetails: getLibraryUserDetails
+	};
 });
 
 app.factory("QuestionFavoriteService", function($q, $http, $timeout){
