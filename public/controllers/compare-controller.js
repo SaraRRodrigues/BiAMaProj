2f5701a964be3f9af5e6906e03fdd8af2beb987c
SaraRRodrigues <sarafof@hotmail.com>
2018-08-17 14:38:09 +0100

    compare user details

--- public/controllers/compare-controller.js
+++ public/controllers/compare-controller.js
@@ -1,4 +1,4 @@
-app.controller("CompareController", ['$scope',"CompareMyMaterialService", "$http", "jQuery", function($scope, CompareMyMaterialService, $http){
+app.controller("CompareController", ['$scope',"CompareMyMaterialService", "UserCompareService", "$http", "jQuery", function($scope, CompareMyMaterialService,UserCompareService, $http){
     
     /* hide footer of index page because of click in buttons footer reload page */
     jQuery("#footerMain").hide();
@@ -51,6 +51,126 @@ app.controller("CompareController", ['$scope',"CompareMyMaterialService", "$http
         window.setTimeout("location.href = 'http://localhost:8080'")
     }
 
+    var splitLocation = location.href.split('=');
+    $scope.idUserLoggerIn =splitLocation[1];
+
+    if($scope.idUserLoggerIn !== undefined) {
+        $scope.confirmSession=true;
+    } else {
+        $scope.loading = true;
+        $scope.confirmSession=false;
+    }
+
+    $scope.logout = function(){
+		$scope.confirmSession = false;
+		/*firebase.auth().signOut().then(function() {
+			// Sign-out successful.
+		
+		}, function(error) {
+			// An error happened.
+			console.log(error);
+
+		});*/
+	}
+    
+    $scope.confirmSessionAction = function (username, password) {
+
+		$scope.users = 'loadUser';
+		var getAllUsers = UserCompareService.getUsers(function(users){});
+		
+		getAllUsers.then(function(usersDB) {
+			$scope.users = usersDB.data.users;
+			for(var index=0; index<$scope.users.length; ++index){
+				$scope.userName = $scope.users[index].username;
+				$scope.userPassword = $scope.users[index].password;
+				$scope.userImage = $scope.users[index].image;
+				$scope.userEmail = $scope.users[index].email;
+				$scope.nameUser=$scope.users[index].name;
+				$scope.userBirthdate = $scope.users[index].birthdate;
+
+				var splitDateBirth = $scope.userBirthdate.split('/');
+				$scope.dayBirth = splitDateBirth[0];
+				$scope.monthBirth = splitDateBirth[1];
+				$scope.yearBirth = splitDateBirth[2];
+
+				if($scope.userName !== null && $scope.userName === username){
+					if($scope.userPassword !== null && $scope.userPassword === password){
+						$scope.userLoggedIn=$scope.users[index].username;
+						$scope.idUserLoggerIn=$scope.users[index].id;
+						$scope.confirmSession = true;
+						break;
+					}
+				}
+			}
+        });
+    }
+
+    $scope.clickTopSearch = function() {
+        if($scope.showSearch){
+            $scope.showSearch = false;
+        }else {
+            $scope.showSearch = true;
+        }
+    }
+
+    $scope.clickUserDetails = function() {
+		if($scope.userDetails){
+			$scope.userDetails = false;
+		}else {
+			$scope.userDetails = true;
+			$scope.showSearch = false;
+		}
+    }
+
+    $scope.showInitSessionDiv = function () {
+		if($scope.showInitSession){
+			$scope.showInitSession = false;
+		}else {
+			$scope.showInitSession = true;
+		}
+    }
+
+    $scope.getRequest = function(buttonClick) {
+		if($scope.isMobileView) {
+			if(buttonClick === 'favorites') {
+				location.href = 'http://localhost:8080/BiAMa/favoritesMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'questions') {
+				location.href = 'http://localhost:8080/BiAMa/myQuestionsMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'world_share') {
+				location.href = 'http://localhost:8080/BiAMa/worldShareMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'notification') {
+				location.href = 'http://localhost:8080/BiAMa/notificationsMobile?userName=' + $scope.idUserLoggerIn;
+	
+			}
+	
+			if(buttonClick == 'perfil') {
+				location.href = 'http://localhost:8080/BiAMa/perfilPageMobile?userId=' + $scope.idUserLoggerIn + '&userName=' 
+				+ $scope.userName + '&userPassword=' + $scope.userPassword + '&userImage=' + $scope.userImage + '&userBirthdate=' + $scope.dayBirth + '-' + $scope.monthBirth + '-' + $scope.yearBirth 
+				+ '&nameUser=' + $scope.nameUser + '&userEmail=' + $scope.userEmail;
+			}
+	
+			if(buttonClick == 'compare') {
+				location.href = 'http://localhost:8080/BiAMa/compareMobile?userName=' + $scope.idUserLoggerIn;
+	
+			}
+		}
+		
+		if(buttonClick === 'notification') {
+			$scope.userDetails = true;
+			$scope.notificationNumber=true;
+		} else {
+			$scope.userDetails = false;
+			$scope.notificationNumber = false;
+		}
+		$scope.search = false;
+	}
+
     $scope.getMaterials.then(function(result) {
         $scope.loading = false;
         var data=result.data.comparationDetails;
@@ -100,4 +220,94 @@ app.factory("CompareMyMaterialService", function($q, $http, $timeout){
     return {
         getMaterialComparation: getMaterialComparation
     };
+});
+
+app.factory("UserCompareService", function($q, $http, $timeout){
+    
+	var getUsers = function() {
+		var deferred = $q.defer();
+	
+ 		$timeout(function() {
+		  deferred.resolve($http.get('/users',  {cache:true}));
+		}, 2000); 
+	
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			deferred.resolve(resp);
+		}
+
+		xhr.open('GET','/users', true);
+		xhr.send();*/
+
+		return deferred.promise;
+	};
+
+	var getMyQuestionsLogged = function() {
+		var deferred = $q.defer();
+
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			deferred.resolve(resp);
+		}
+
+		xhr.open('GET','/myQuest', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.get('/myQuest'));
+		}, 2000);
+
+		return deferred.promise;
+	};
+
+	var insertLibraryUserDetails = function() {
+		var deferred = $q.defer();
+
+		
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			var response = resp.response;
+			deferred.resolve(response);
+		}
+
+		xhr.open('GET','/insertLibraryUser', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.post('/insertLibraryUser'));
+		}, 2000);
+
+		return deferred.promise;
+	}
+
+	var getLibraryUserDetails = function() {
+		
+		var deferred = $q.defer();
+
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			var response = resp.response;
+			deferred.resolve(response);
+		}
+
+		xhr.open('GET','/getLibraryUser', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.get('/getLibraryUser'));
+		}, 2000);
+
+		return deferred.promise;
+	}
+
+	return {
+		getUsers: getUsers,
+		getMyQuestionsLogged: getMyQuestionsLogged,
+		insertLibraryUserDetails: insertLibraryUserDetails,
+		getLibraryUserDetails: getLibraryUserDetails
+	};
 });
--- public/controllers/perfil-controller.js
+++ public/controllers/perfil-controller.js
@@ -1,5 +1,5 @@
 
-app.controller("PerfilController", ['$scope', "$http", "jQuery", function($scope, $http){
+app.controller("PerfilController", ['$scope',"UserPerfilService", "$http", "jQuery", function($scope,UserPerfilService, $http){
 
     /* hide footer of index page because of click in buttons footer reload page */
     jQuery("#footerMain").hide();
@@ -25,6 +25,30 @@ app.controller("PerfilController", ['$scope', "$http", "jQuery", function($scope
     $scope.uploadPhoto='';
     $scope.new_birthdate='';
     $scope.showPerfilDetails=false;
+    $scope.loading=true;
+
+    var getAllUsers = UserPerfilService.getUsers(function(users){});
+    getAllUsers.then(function(usersDB) {
+        $scope.loading=false;
+        $scope.users = usersDB.data.users;
+        for(var index=0; index<$scope.users.length; ++index){
+            $scope.username = $scope.users[index].username;
+            $scope.password = $scope.users[index].password;
+            $scope.imageUser = $scope.users[index].image;
+            $scope.email = $scope.users[index].email;
+            $scope.name=$scope.users[index].name;
+            $scope.birthdateValue = $scope.users[index].birthdate;
+
+            if($scope.userName !== null && $scope.userName === username){
+                if($scope.userPassword !== null && $scope.userPassword === password){
+                    $scope.userLoggedIn=$scope.users[index].username;
+                    $scope.idUserLoggerIn=$scope.users[index].id;
+                    $scope.confirmSession = true;
+                    break;
+                }
+            }
+        }
+    });
 
     $scope.getData = function() {
 
@@ -60,19 +84,131 @@ app.controller("PerfilController", ['$scope', "$http", "jQuery", function($scope
 
     $scope.initUserDetails();
 
+    var splitLocation = location.href.split('=');
+    $scope.idUserLoggerIn =splitLocation[1];
+
+    if($scope.idUserLoggerIn !== undefined) {
+        $scope.confirmSession=true;
+    } else {
+        $scope.loading = true;
+        $scope.confirmSession=false;
+    }
+
+    $scope.logout = function(){
+		$scope.confirmSession = false;
+		/*firebase.auth().signOut().then(function() {
+			// Sign-out successful.
+		
+		}, function(error) {
+			// An error happened.
+			console.log(error);
+
+		});*/
+	}
+    
+    $scope.confirmSessionAction = function (username, password) {
+
+		$scope.users = 'loadUser';
+		var getAllUsers = UserPerfilService.getUsers(function(users){});
+		
+		getAllUsers.then(function(usersDB) {
+			$scope.users = usersDB.data.users;
+			for(var index=0; index<$scope.users.length; ++index){
+				$scope.userName = $scope.users[index].username;
+				$scope.userPassword = $scope.users[index].password;
+				$scope.userImage = $scope.users[index].image;
+				$scope.userEmail = $scope.users[index].email;
+				$scope.nameUser=$scope.users[index].name;
+				$scope.userBirthdate = $scope.users[index].birthdate;
+
+				var splitDateBirth = $scope.userBirthdate.split('/');
+				$scope.dayBirth = splitDateBirth[0];
+				$scope.monthBirth = splitDateBirth[1];
+				$scope.yearBirth = splitDateBirth[2];
+
+				if($scope.userName !== null && $scope.userName === username){
+					if($scope.userPassword !== null && $scope.userPassword === password){
+						$scope.userLoggedIn=$scope.users[index].username;
+						$scope.idUserLoggerIn=$scope.users[index].id;
+						$scope.confirmSession = true;
+						break;
+					}
+				}
+			}
+        });
+    }
+
     $scope.clickTopSearch = function() {
-		if($scope.showSearch){
-			$scope.showSearch = false;
+        if($scope.showSearch){
+            $scope.showSearch = false;
+        }else {
+            $scope.showSearch = true;
+        }
+    }
+
+    $scope.clickUserDetails = function() {
+		if($scope.userDetails){
+			$scope.userDetails = false;
 		}else {
-			$scope.showSearch = true;
+			$scope.userDetails = true;
+			$scope.showSearch = false;
+		}
+    }
+
+    $scope.showInitSessionDiv = function () {
+		if($scope.showInitSession){
+			$scope.showInitSession = false;
+		}else {
+			$scope.showInitSession = true;
 		}
     }
     
     $scope.goToHomePage = function() {
         window.setTimeout("location.href = 'http://localhost:8080'")
     }
+
+    $scope.getRequest = function(buttonClick) {
+		if($scope.isMobileView) {
+			if(buttonClick === 'favorites') {
+				location.href = 'http://localhost:8080/BiAMa/favoritesMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'questions') {
+				location.href = 'http://localhost:8080/BiAMa/myQuestionsMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'world_share') {
+				location.href = 'http://localhost:8080/BiAMa/worldShareMobile?userName=' + $scope.idUserLoggerIn;
+			}
+	
+			if(buttonClick == 'notification') {
+				location.href = 'http://localhost:8080/BiAMa/notificationsMobile?userName=' + $scope.idUserLoggerIn;
+	
+			}
+	
+			if(buttonClick == 'perfil') {
+				location.href = 'http://localhost:8080/BiAMa/perfilPageMobile?userId=' + $scope.idUserLoggerIn + '&userName=' 
+				+ $scope.userName + '&userPassword=' + $scope.userPassword + '&userImage=' + $scope.userImage + '&userBirthdate=' + $scope.dayBirth + '-' + $scope.monthBirth + '-' + $scope.yearBirth 
+				+ '&nameUser=' + $scope.nameUser + '&userEmail=' + $scope.userEmail;
+			}
+	
+			if(buttonClick == 'compare') {
+				location.href = 'http://localhost:8080/BiAMa/compareMobile?userName=' + $scope.idUserLoggerIn;
+	
+			}
+		}
+		
+		if(buttonClick === 'notification') {
+			$scope.userDetails = true;
+			$scope.notificationNumber=true;
+		} else {
+			$scope.userDetails = false;
+			$scope.notificationNumber = false;
+		}
+		$scope.search = false;
+	}
     
-    $scope.clickUserDetails = function() {
+    $scope.updateData = function() {
         $scope.birthdateValue='';
         $scope.editDate=true;
         $scope.upgradeInformations=true;
@@ -205,7 +341,97 @@ app.controller("PerfilController", ['$scope', "$http", "jQuery", function($scope
         $scope.openImageUploadLabel=true;
     }
 
-    if($scope.users === undefined) {
+    if($scope.isMobileView) {
         $scope.getData();
     }
 }])
+
+app.factory("UserPerfilService", function($q, $http, $timeout){
+    
+	var getUsers = function() {
+		var deferred = $q.defer();
+	
+ 		$timeout(function() {
+		  deferred.resolve($http.get('/users',  {cache:true}));
+		}, 2000); 
+	
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			deferred.resolve(resp);
+		}
+
+		xhr.open('GET','/users', true);
+		xhr.send();*/
+
+		return deferred.promise;
+	};
+
+	var getMyQuestionsLogged = function() {
+		var deferred = $q.defer();
+
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			deferred.resolve(resp);
+		}
+
+		xhr.open('GET','/myQuest', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.get('/myQuest'));
+		}, 2000);
+
+		return deferred.promise;
+	};
+
+	var insertLibraryUserDetails = function() {
+		var deferred = $q.defer();
+
+		
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			var response = resp.response;
+			deferred.resolve(response);
+		}
+
+		xhr.open('GET','/insertLibraryUser', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.post('/insertLibraryUser'));
+		}, 2000);
+
+		return deferred.promise;
+	}
+
+	var getLibraryUserDetails = function() {
+		
+		var deferred = $q.defer();
+
+		/*var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var resp = this;
+			var response = resp.response;
+			deferred.resolve(response);
+		}
+
+		xhr.open('GET','/getLibraryUser', true);
+		xhr.send();*/
+
+		$timeout(function() {
+			deferred.resolve($http.get('/getLibraryUser'));
+		}, 2000);
+
+		return deferred.promise;
+	}
+
+	return {
+		getUsers: getUsers,
+		getMyQuestionsLogged: getMyQuestionsLogged,
+		insertLibraryUserDetails: insertLibraryUserDetails,
+		getLibraryUserDetails: getLibraryUserDetails
+	};
+});
