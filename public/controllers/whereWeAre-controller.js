9e500e991fee3098d08bd4a92d1c61b5e32fd757
SaraRRodrigues <sarafof@hotmail.com>
2018-08-22 00:08:15 +0100

    where we are mini search - mobile view

--- public/controllers/whereWeAre-controller.js
+++ public/controllers/whereWeAre-controller.js
@@ -1,4 +1,4 @@
-app.controller("WhereWeAreController", ['$scope',  "BiAMaInfoService", "LibraryMaterialInfoService", "$http" ,"$sce", "$route", "jQuery", "$location", function($scope, BiAMaInfoService, LibraryMaterialInfoService, $http, $sce, $route, $location){
+app.controller("WhereWeAreController", ['$scope',  "BiAMaInfoService", "LibraryMaterialInfoService", "WhereWeAreMaterialService", "$http" ,"$sce", "$route", "jQuery", "$location", function($scope, BiAMaInfoService, LibraryMaterialInfoService, WhereWeAreMaterialService, $http, $sce, $route, $location){
    
     /* hide footer of index page because of click in buttons footer reload page */
     jQuery("#footerMain").hide();
@@ -15,8 +15,20 @@ app.controller("WhereWeAreController", ['$scope',  "BiAMaInfoService", "LibraryM
     
     $scope.loading = true;
     $scope.schools=[];
+    $scope.resultSearch = [];
+    $scope.showDetailsOfMaterial=false;
+    $scope.miniSearchResults=false;
+    $scope.showLocation=true;
     $scope.pathURL='https://www.google.com/maps/';
 
+	$scope.getMaterials = WhereWeAreMaterialService.getMaterialComparation(function(infoMaterial){});
+	$scope.getMaterials.then(function(result) {
+		$scope.loading = false;
+		var data=result.data.comparationDetails;
+		$scope.materialsToSearch = data;
+
+    });
+    
     $scope.goToHomePage = function() {
         window.setTimeout("location.href = 'http://localhost:8080'")
     }
@@ -26,6 +38,37 @@ app.controller("WhereWeAreController", ['$scope',  "BiAMaInfoService", "LibraryM
 		$scope.search = false;
 		$scope.nameclick=name;
     }
+
+    $scope.initMiniSearch = function() {
+
+		var inputMini = jQuery("#miniSearch").val();
+		if(inputMini !== '') {
+			for(var index=0; index < $scope.materialsToSearch.length; ++index) {
+				var resultMaterial = {
+					'name': $scope.materialsToSearch[index].name,
+					'category': $scope.materialsToSearch[index].category,
+					'description': $scope.materialsToSearch[index].description,
+					'code': $scope.materialsToSearch[index].code
+				}
+				if(($scope.materialsToSearch[index].type).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push(resultMaterial);
+				} else if(($scope.materialsToSearch[index].color).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push(resultMaterial);
+				} else if(($scope.materialsToSearch[index].category).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push(resultMaterial);
+				} else if(($scope.materialsToSearch[index].description).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push($scope.materialsToSearch[index].name);
+				}
+			}
+	
+			$scope.showInitSearch=false;
+			$scope.miniSearchResults = true;
+
+			$scope.showCategory=true;
+            $scope.showMaterialDetails=false;
+            $scope.showLocation=false;
+		}
+	}
     
     $scope.clickTopSearch = function() {
 		if($scope.showSearch){
@@ -57,6 +100,32 @@ app.controller("WhereWeAreController", ['$scope',  "BiAMaInfoService", "LibraryM
 		}
     }
 
+    $scope.closeMaterial = function(){
+		$scope.miniSearchResults=true;
+        $scope.showDetailsOfMaterial=false;
+        $scope.showLocation=true;
+	}
+
+    $scope.closeMiniSearch = function() {
+		$scope.miniSearchResults = false;
+		$scope.search=true;
+		$scope.openMaterialDetail=false; 
+		$scope.showInitSearch=true;
+		$scope.showSearch=false;
+		$scope.enableUserIcon=false;
+		$scope.showCategory=false;
+        $scope.showMaterialDetails=false;
+        $scope.showLocation=true;
+	}
+
+	$scope.openMaterial = function(material) {
+		$scope.showDetailsOfMaterial=true;
+		$scope.showMaterials=false;
+		$scope.openedMaterial=material;
+        $scope.miniSearchResults=false;
+        $scope.showLocation=false;
+    }
+    
     $scope.selectSchool = function (locationSchool) {
         for(var index=0; index <$scope.biamaDetails.length; ++index) {
             if($scope.biamaDetails[index].locationDescription === locationSchool){
@@ -91,4 +160,35 @@ app.factory("BiAMaInfoService", function($q, $http, $timeout){
 	  return {
 		getBiAMaInfo: getBiAMaInfo
 	  };
+});
+
+app.factory("WhereWeAreMaterialService", function($q, $http, $timeout){
+	var getMaterialComparation = function() {
+			var deferred = $q.defer();
+
+	/*var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		var resp = this;
+		if (this.readyState == 4 && this.status == 200) {
+			var response = resp.response;
+			debugger
+			deferred.resolve(response);
+		}
+		
+	}
+
+	xhr.open('GET','/compareMaterials', true);
+	xhr.send();*/
+
+			$timeout(function() {
+			deferred.resolve($http.get('/compareMaterials'));
+			}, 4000);
+
+			return deferred.promise;
+	};
+
+
+	return {
+			getMaterialComparation: getMaterialComparation
+	};
 });
