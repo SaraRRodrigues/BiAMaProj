2f247909cd6fe619b9edccde3160211bec585f89
SaraRRodrigues <sarafof@hotmail.com>
2018-08-22 00:28:59 +0100

    qr code mini search - mobile view

--- public/controllers/qrCode-controller.js
+++ public/controllers/qrCode-controller.js
@@ -1,13 +1,25 @@
 
-app.controller("QrCodeController", ['$scope',"$http", "jQuery",function($scope, $http){
+app.controller("QrCodeController", ['$scope',"QrCodeMaterialService","$http", "jQuery",function($scope, QrCodeMaterialService,$http){
 
 	/* hide footer of index page because of click in buttons footer reload page */
 	jQuery("#footerMain").hide();
 	/* my current page */
     $scope.namePage='qrCode';
     $scope.showSearch = false;
-    $scope.userDetails = false;
-    
+	$scope.userDetails = false;
+	$scope.miniSearchResults=false;
+	$scope.showDetailsOfMaterial=false;
+	$scope.resultSearch= [];
+	$scope.showQrCode = true;
+	
+	$scope.getMaterials = QrCodeMaterialService.getMaterialComparation(function(infoMaterial){});
+	$scope.getMaterials.then(function(result) {
+		$scope.loading = false;
+		var data=result.data.comparationDetails;
+		$scope.materialsToSearch = data;
+
+	});
+	
     $scope.clickTopSearch = function() {
 		if($scope.showSearch){
 			$scope.showSearch = false;
@@ -19,4 +31,97 @@ app.controller("QrCodeController", ['$scope',"$http", "jQuery",function($scope, 
     $scope.goToHomePage = function() {
         window.setTimeout("location.href = 'http://localhost:8080'")
 	}
+
+	$scope.openMaterial = function(material) {
+		$scope.showDetailsOfMaterial=true;
+		$scope.showMaterials=false;
+		$scope.openedMaterial=material;
+        $scope.miniSearchResults=false;
+		$scope.showLocation=false;
+		$scope.showQrCode = false;
+	}
+	
+	$scope.closeMaterial = function(){
+		$scope.miniSearchResults=true;
+        $scope.showDetailsOfMaterial=false;
+		$scope.showLocation=true;
+		$scope.showQrCode = true;
+	}
+
+    $scope.closeMiniSearch = function() {
+		$scope.miniSearchResults = false;
+		$scope.search=true;
+		$scope.openMaterialDetail=false; 
+		$scope.showInitSearch=true;
+		$scope.showSearch=false;
+		$scope.enableUserIcon=false;
+		$scope.showCategory=false;
+        $scope.showMaterialDetails=false;
+		$scope.showLocation=true;
+		$scope.showQrCode = true;
+	}
+
+	$scope.initMiniSearch = function() {
+
+		var inputMini = jQuery("#miniSearch").val();
+		if(inputMini !== '') {
+			for(var index=0; index < $scope.materialsToSearch.length; ++index) {
+				var resultMaterial = {
+					'name': $scope.materialsToSearch[index].name,
+					'category': $scope.materialsToSearch[index].category,
+					'description': $scope.materialsToSearch[index].description,
+					'code': $scope.materialsToSearch[index].code
+				}
+				if(($scope.materialsToSearch[index].type).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push(resultMaterial);
+				} else if(($scope.materialsToSearch[index].color).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push(resultMaterial);
+				} else if(($scope.materialsToSearch[index].category).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push(resultMaterial);
+				} else if(($scope.materialsToSearch[index].description).toLowerCase().indexOf(inputMini) !== -1) {
+					$scope.resultSearch.push($scope.materialsToSearch[index].name);
+				}
+			}
+	
+			$scope.showInitSearch=false;
+			$scope.miniSearchResults = true;
+
+			$scope.showCategory=true;
+            $scope.showMaterialDetails=false;
+			$scope.showLocation=false;
+			$scope.showQrCode = false;
+		}
+	}
+    
 }])
+
+app.factory("QrCodeMaterialService", function($q, $http, $timeout){
+	var getMaterialComparation = function() {
+			var deferred = $q.defer();
+
+	/*var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		var resp = this;
+		if (this.readyState == 4 && this.status == 200) {
+			var response = resp.response;
+			debugger
+			deferred.resolve(response);
+		}
+		
+	}
+
+	xhr.open('GET','/compareMaterials', true);
+	xhr.send();*/
+
+			$timeout(function() {
+			deferred.resolve($http.get('/compareMaterials'));
+			}, 4000);
+
+			return deferred.promise;
+	};
+
+
+	return {
+			getMaterialComparation: getMaterialComparation
+	};
+});
