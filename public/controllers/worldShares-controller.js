de17f019bcc8cf664b23cd154b7dca971fb2f030
SaraRRodrigues <sarafof@hotmail.com>
2018-08-22 11:06:42 +0100

    my world shares mini search - mobile view

--- public/controllers/worldShares-controller.js
+++ public/controllers/worldShares-controller.js
@@ -1,4 +1,4 @@
-app.controller("WorldShareController", ['$scope',"WorldSharesService", "ForumService","UserWorldShareService", "$http", "jQuery", function($scope,WorldSharesService,ForumService,UserWorldShareService,$http){
+app.controller("WorldShareController", ['$scope',"WorldSharesService", "ForumService","UserWorldShareService", "WorldShareMaterialService", "$http", "jQuery", function($scope,WorldSharesService,ForumService,UserWorldShareService, WorldShareMaterialService,$http){
     
     /* hide footer of index page because of click in buttons footer reload page */
     jQuery("#footerMain").hide();
@@ -13,6 +13,10 @@ app.controller("WorldShareController", ['$scope',"WorldSharesService", "ForumSer
     $scope.descriptionWorldShare='';
     $scope.savePhoto=false;
     $scope.openWorldShareUploadLabel=false;
+
+    $scope.resultSearch = [];
+    $scope.showDetailsOfMaterial=false;
+    $scope.miniSearchResults=false;
 
     var window_width = $( window ).width();
 	if(window_width <= 1024) {
@@ -31,6 +35,80 @@ app.controller("WorldShareController", ['$scope',"WorldSharesService", "ForumSer
         $scope.confirmSession=false;
     }
 
+    $scope.getMaterials = WorldShareMaterialService.getMaterialComparation(function(infoMaterial){});
+    $scope.getMaterials.then(function(result) {
+      $scope.loading = false;
+      var data=result.data.comparationDetails;
+      $scope.materialsToSearch = data;
+  
+      });
+
+    $scope.initMiniSearch = function() {
+
+      var inputMini = jQuery("#miniSearch").val();
+      if(inputMini !== '') {
+        for(var index=0; index < $scope.materialsToSearch.length; ++index) {
+          var resultMaterial = {
+            'name': $scope.materialsToSearch[index].name,
+            'category': $scope.materialsToSearch[index].category,
+            'description': $scope.materialsToSearch[index].description,
+            'code': $scope.materialsToSearch[index].code
+          }
+          if(($scope.materialsToSearch[index].type).toLowerCase().indexOf(inputMini) !== -1) {
+            $scope.resultSearch.push(resultMaterial);
+          } else if(($scope.materialsToSearch[index].color).toLowerCase().indexOf(inputMini) !== -1) {
+            $scope.resultSearch.push(resultMaterial);
+          } else if(($scope.materialsToSearch[index].category).toLowerCase().indexOf(inputMini) !== -1) {
+            $scope.resultSearch.push(resultMaterial);
+          } else if(($scope.materialsToSearch[index].description).toLowerCase().indexOf(inputMini) !== -1) {
+            $scope.resultSearch.push($scope.materialsToSearch[index].name);
+          }
+        }
+    
+        $scope.showInitSearch=false;
+        $scope.miniSearchResults = true;
+
+        $scope.showMaterialDetails=false;
+        $scope.showForum = false;
+        $scope.showQuestionDetails=false;
+
+        $scope.showWorldShares=false;
+      }
+    }
+
+    $scope.closeMaterial = function(){
+      $scope.miniSearchResults=false;
+      $scope.showDetailsOfMaterial=false; 
+      $scope.showWorldShares=true;
+
+    }
+  
+    $scope.closeMiniSearch = function() {
+      $scope.miniSearchResults = false;
+      $scope.search=true;
+      $scope.openMaterialDetail=false; 
+      $scope.showInitSearch=true;
+      $scope.showSearch=false;
+      $scope.enableUserIcon=false;
+      $scope.showWorldShares=true;
+    }
+  
+    $scope.openMaterial = function(material) {
+      $scope.miniSearchResults=false;
+      $scope.showDetailsOfMaterial=true;
+      $scope.showMaterials=false;
+      $scope.openedMaterial=material;
+      $scope.showWorldShares=false;
+    }
+
+    $scope.clickTopSearch = function() {
+		if($scope.showSearch){
+			$scope.showSearch = false;
+		}else {
+			$scope.showSearch = true;
+		}
+    }
+    
     $scope.logout = function(){
 		$scope.confirmSession = false;
 		/*firebase.auth().signOut().then(function() {
@@ -387,4 +465,35 @@ app.factory("UserWorldShareService", function($q, $http, $timeout){
 		insertLibraryUserDetails: insertLibraryUserDetails,
 		getLibraryUserDetails: getLibraryUserDetails
 	};
+});
+
+app.factory("WorldShareMaterialService", function($q, $http, $timeout){
+    var getMaterialComparation = function() {
+        var deferred = $q.defer();
+
+    /*var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      var resp = this;
+      if (this.readyState == 4 && this.status == 200) {
+        var response = resp.response;
+        debugger
+        deferred.resolve(response);
+      }
+      
+    }
+
+    xhr.open('GET','/compareMaterials', true);
+    xhr.send();*/
+
+        $timeout(function() {
+        deferred.resolve($http.get('/compareMaterials'));
+        }, 4000);
+
+        return deferred.promise;
+    };
+
+
+    return {
+        getMaterialComparation: getMaterialComparation
+    };
 });
