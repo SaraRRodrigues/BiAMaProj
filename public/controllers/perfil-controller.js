4c05bf3fdcaf365af50ee2f8329c47a4f3e26ff2
SaraRRodrigues <sarafof@hotmail.com>
2018-09-16 11:50:37 +0100

    refator code on perfil page

--- public/controllers/perfil-controller.js
+++ public/controllers/perfil-controller.js
@@ -85,22 +85,23 @@ app.controller("PerfilController", ['$scope', "UserPerfilService", "PerfilMateri
             $scope.loading=false;
             $scope.users = usersDB.data.users;
             for(var index=0; index<$scope.users.length; ++index){
-                $scope.username = $scope.users[index].username;
-                $scope.password = $scope.users[index].password;
                
-                if($scope.userName !== null && $scope.userName === $scope.username){
-                    if($scope.userPassword !== null && $scope.userPassword === $scope.password){
-                        $scope.userLoggedIn=$scope.users[index].username;
-                        $scope.idUserLoggerIn=$scope.users[index].id;
-                        $scope.confirmSession = true;
-                        $scope.imageUser = $scope.users[index].image;
-                        $scope.email = $scope.users[index].email;
-                        $scope.name=$scope.users[index].name;
-                        $scope.birthdateValue = $scope.users[index].birthdate;        
-                        
-                    }
-                    break;
-                } else {
+                if($scope.users[index].id === $scope.idUserLoggerIn) {
+                    $scope.userName = $scope.users[index].username;
+                    $scope.userPassword = $scope.users[index].password;
+                    $scope.userLoggedIn=$scope.users[index].username;
+                    $scope.idUserLoggerIn=$scope.users[index].id;
+                    $scope.confirmSession = true;
+                    
+                    $scope.userImage = $scope.users[index].image;
+                    $scope.userEmail = $scope.users[index].email;
+                    $scope.nameUser=$scope.users[index].name;
+                    $scope.userBirthdate = $scope.users[index].birthdate;
+
+                    var splitDateBirth = $scope.userBirthdate.split('/');
+                    $scope.dayBirth = splitDateBirth[0];
+                    $scope.monthBirth = splitDateBirth[1];
+                    $scope.yearBirth = splitDateBirth[2];
                     break;
                 }
             }
