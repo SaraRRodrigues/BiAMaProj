b32f631b3330ab1aba903f4ec398abb00b32a580
SaraRRodrigues <sarafof@hotmail.com>
2018-09-19 08:10:25 +0100

    favorites refator

--- public/controllers/favorites-controller.js
+++ public/controllers/favorites-controller.js
@@ -218,17 +218,19 @@ app.controller('FavoritesController',['$scope', "$http", "FavoritesService", "Li
     /* open favorites button */
     $scope.openFavoritesButton = function(){
         /* get favorites in materials */
-        for(var index=0; index<$scope.materialsCategories.length; ++index){
-            for(var indexFav=0; indexFav<$scope.favoriteMaterials.length; ++indexFav) {
-                if($scope.materialsCategories[index].material_id === $scope.favoriteMaterials[indexFav].material_id) {
-                    var infoFav = {
-                        "materialId": $scope.materialsCategories[index].material_id,
-                        "description": $scope.materialsCategories[index].description,
-                        "category": $scope.materialsCategories[index].category,
-                        "image": $scope.materialsCategories[index].name,
-                        "isFavorite": true
+        if($scope.favorites.length == 0) {
+            for(var index=0; index<$scope.materialsCategories.length; ++index){
+                for(var indexFav=0; indexFav<$scope.favoriteMaterials.length; ++indexFav) {
+                    if($scope.materialsCategories[index].material_id === $scope.favoriteMaterials[indexFav].material_id) {
+                        var infoFav = {
+                            "materialId": $scope.materialsCategories[index].material_id,
+                            "description": $scope.materialsCategories[index].description,
+                            "category": $scope.materialsCategories[index].category,
+                            "image": $scope.materialsCategories[index].name,
+                            "isFavorite": true
+                        }
+                        $scope.favorites.push(infoFav)
                     }
-                    $scope.favorites.push(infoFav)
                 }
             }
         }
